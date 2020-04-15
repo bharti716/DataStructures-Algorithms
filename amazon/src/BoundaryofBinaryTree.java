eb390f376c65028fdf108d822c26e7f6a938b1de
Bharti Singh <bharti.singh@flipkart.com>
2020-04-15 11:11:09 +0530

    Added more questions

--- amazon/src/BoundaryofBinaryTree.java
+++ amazon/src/BoundaryofBinaryTree.java
@@ -1,21 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.  (The values of the nodes may still be duplicates.)
- Left boundary is defined as the path from root to the left-most node. Right boundary is defined as the path from root to the right-most node. If the root doesn't have left subtree or right subtree, then the root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
- The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. If not, travel to the right subtree. Repeat until you reach a leaf node.
- The right-most node is also defined by the same way with left and right exchanged.
-
- Input:
-  1
-  \
-  2
- / \
- 3  4
-
- Ouput:
- [1, 3, 4, 2]
-
-
+ * Given a binary tree, return the values of its boundary in anti-clockwise direction starting from root. Boundary includes left boundary, leaves, and right boundary in order without duplicate nodes.  (The values of the nodes may still be duplicates.)
+ * Left boundary is defined as the path from root to the left-most node. Right boundary is defined as the path from root to the right-most node. If the root doesn't have left subtree or right subtree, then the root itself is left boundary or right boundary. Note this definition only applies to the input binary tree, and not applies to any subtrees.
+ * The left-most node is defined as a leaf node you could reach when you always firstly travel to the left subtree if exists. If not, travel to the right subtree. Repeat until you reach a leaf node.
+ * The right-most node is also defined by the same way with left and right exchanged.
+ * <p>
+ * Input:
+ * 1
+ * \
+ * 2
+ * / \
+ * 3  4
+ * <p>
+ * Ouput:
+ * [1, 3, 4, 2]
  */
 
 public class BoundaryofBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        res.add(root.val);
+        left(root.left, res);
+        leaves(root.left, res);
+        leaves(root.right, res);
+        right(root.right, res);
+        return res;
+    }
+
+    private void left(TreeNode root, List<Integer> res) {
+        if (root == null || (root.left == null && root.right == null)) return;
+        res.add(root.val);
+        if (root.left == null)
+            left(root.right, res);
+        else
+            left(root.left, res);
+    }
+
+    private void right(TreeNode root, List<Integer> res) {
+        if (root == null || (root.left == null && root.right == null)) return;
+        if (root.right == null)
+            right(root.left, res);
+        else
+            right(root.right, res);
+        res.add(root.val);
+    }
+
+    private void leaves(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        if (root.left == null && root.right == null)
+            res.add(root.val);
+        leaves(root.left, res);
+        leaves(root.right, res);
+    }
 }
--- linkedIn/src/LRUCache.java
+++ linkedIn/src/LRUCache.java
@@ -1,30 +1,101 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
-
- get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
- put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
-
- The cache is initialized with a positive capacity.
-
- Follow up:
- Could you do both operations in O(1) time complexity?
-
- Example:
-
- LRUCache cache = new LRUCache( 2 ) -- 2 is capacity
-
-        cache.put(1, 1);
-        cache.put(2, 2);
-        cache.get(1);       // returns 1
-        cache.put(3, 3);    // evicts key 2
-        cache.get(2);       // returns -1 (not found)
-        cache.put(4, 4);    // evicts key 1
-        cache.get(1);       // returns -1 (not found)
-        cache.get(3);       // returns 3
-        cache.get(4);       // returns 4
-
+ * Design and implement a data structure for Least Recently Used (LRU) cache. It should support the following operations: get and put.
+ * <p>
+ * get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
+ * put(key, value) - Set or insert the value if the key is not already present. When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+ * <p>
+ * The cache is initialized with a positive capacity.
+ * <p>
+ * Follow up:
+ * Could you do both operations in O(1) time complexity?
+ * <p>
+ * Example:
+ * <p>
+ * LRUCache cache = new LRUCache( 2 ) -- 2 is capacity
+ * <p>
+ * cache.put(1, 1);
+ * cache.put(2, 2);
+ * cache.get(1);       // returns 1
+ * cache.put(3, 3);    // evicts key 2
+ * cache.get(2);       // returns -1 (not found)
+ * cache.put(4, 4);    // evicts key 1
+ * cache.get(1);       // returns -1 (not found)
+ * cache.get(3);       // returns 3
+ * cache.get(4);       // returns 4
  */
 public class LRUCache {
+    class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
 
+        public Node(int k, int v) {
+            key = k;
+            val = v;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private Map<Integer, Node> map;
+    private int capacity;
+    private int size;
+
+    public LRUCache(int capacity) {
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        map = new HashMap<>();
+        head.next = tail;
+        tail.prev = head;
+        this.capacity = capacity;
+        size = 0;
+    }
+
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            remove(key);
+            addHead(key, node.val);
+            return node.val;
+        } else return -1;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            remove(key);
+            addHead(key, value);
+        } else {
+            addHead(key, value);
+        }
+    }
+
+    private void remove(int key) {
+        Node cur = map.get(key);
+        Node prev = cur.prev;
+        Node next = cur.next;
+        prev.next = next;
+        next.prev = prev;
+        size--;
+        map.remove(key);
+    }
+
+    private void addHead(int key, int val) {
+        Node node = new Node(key, val);
+        Node next = head.next;
+        head.next = node;
+        node.next = next;
+        next.prev = node;
+        node.prev = head;
+        map.put(key, node);
+        size++;
+        if (size > capacity) {
+            Node preTail = tail.prev;
+            remove(preTail.key);
+        }
+    }
 
 }
